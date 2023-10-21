130b60ada5f10af65b1401ae4888a4b6ca8b6c4c
Matias Soto <matias.soto.26@gmail.com>
2023-10-20 23:24:00 -0300

    Card image feature and some style fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Card from "@/components/card";
 
 export default function Home() {
-  const [cards, setCards] = useState([
-    { id: 1, flipped: false, matched: false },
-    { id: 2, flipped: false, matched: false },
-    { id: 3, flipped: false, matched: false },
-    { id: 4, flipped: false, matched: false },
-    { id: 5, flipped: false, matched: false },
-    { id: 6, flipped: false, matched: false },
-    { id: 7, flipped: false, matched: false },
-    { id: 8, flipped: false, matched: false },
-    { id: 9, flipped: false, matched: false },
-    { id: 10, flipped: false, matched: false },
-    { id: 11, flipped: false, matched: false },
-    { id: 12, flipped: false, matched: false },
-    { id: 13, flipped: false, matched: false },
-    { id: 14, flipped: false, matched: false },
-    { id: 15, flipped: false, matched: false },
-    { id: 16, flipped: false, matched: false },
-    { id: 17, flipped: false, matched: false },
-    { id: 18, flipped: false, matched: false },
-  ]);
+  const [cards, setCards] = useState([]);
   const [flippedCards, setFlippedCards] = useState([]);
 
   const handleCardClick = (id) => {
     if (flippedCards.length === 2) {
+      return;
+    }
+    if (flippedCards.includes(id)) {
       return;
     }
     const newCards = cards.map((card) => {
@@ -44,7 +29,7 @@ export default function Home() {
     }
     const firstMatchedCard = cards.find((card) => card.id === flippedCards[0]);
     const secondMatchedCard = cards.find((card) => card.id === flippedCards[1]);
-    if (firstMatchedCard.id === secondMatchedCard.id + 9 || firstMatchedCard.id === secondMatchedCard.id - 9) {
+    if (firstMatchedCard.animal === secondMatchedCard.animal || firstMatchedCard.animal === secondMatchedCard.animal) {
       const newCards = cards.map((card) => {
         if (card.id === firstMatchedCard.id || card.id === secondMatchedCard.id) {
           return { ...card, matched: true };
@@ -68,19 +53,36 @@ export default function Home() {
     setFlippedCards([]);
   }, [flippedCards]);
 
+  const fetchImages = async () => {
+    const response = await fetch("https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20");
+    const data = await response.json();
+    const images = data.entries.map((image) => {
+      return { flipped: false, matched: false, image: image.fields.image.url, animal: image.meta.name };
+    });
+
+    const shuffledImages = images.sort(() => 0.5 - Math.random());
+    const selectedImages = shuffledImages.slice(0, 9);
+
+    const duplicatedImages = [...selectedImages, ...selectedImages];
+    const imagesWithIds = duplicatedImages.map((image, index) => {
+      return { ...image, id: index + 1 };
+    });
+
+    setCards(imagesWithIds.sort(() => 0.5 - Math.random()));
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
-    <main className="memory-game">
-      {cards.map((card) => (
-        <div
-          key={card.id}
-          className={`memory-card ${card.flipped ? "flip" : ""} ${card.matched ? "match" : ""
-            }`}
-          onClick={() => handleCardClick(card.id)}
-        >
-          <div className="front-face">Front</div>
-          <div className="back-face">{card.id}</div>
-        </div>
-      ))}
-    </main>
+    <div className="page-container">
+      <h1 className="title">Memory Game</h1>
+      <div className="memory-game">
+        {cards.map((card) => (
+          <Card key={card.id} card={card} handleCardClick={handleCardClick} />
+        ))}
+      </div>
+    </div>
   );
 }
